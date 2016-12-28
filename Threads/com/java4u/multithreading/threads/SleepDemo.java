133dc22dadb9ced18275385b7bdc4ff3f212f7c0
arun kumar <arunkumar.varada@gmail.com>
2016-12-28 11:52:00 +0530

    first commit

---
+++ Threads/com/java4u/multithreading/threads/SleepDemo.java
@@ -0,0 +1,14 @@
+package com.java4u.multithreading.threads;
+
+public class SleepDemo implements Runnable {
+
+	public static void main(String[] args) {
+
+	}
+
+	@Override
+	public void run() {
+
+	}
+
+}
